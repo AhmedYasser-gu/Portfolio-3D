df84ed511606fdb47e396dff1c5b46b08fd4b1d4
AhmedYasser-gu <gusyasah@student.gu.se>
2022-06-20 10:48:31 +0200

    Added Audio for bg

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -89,6 +89,21 @@ const moon = new THREE.Mesh(
   })
 );
 
+const listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+const sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load( 'Hymn For The Weekend.mp3', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop( true );
+	sound.setVolume( 0.5 );
+	sound.play();
+});
+
 scene.add(moon);
 
 moon.position.z = 25;
